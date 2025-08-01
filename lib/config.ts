1e1360fb2150a7d3105eefd4ecde2e9af4a15bd8
Alexis <alexisbouley.ai@gmail.com>
2025-08-01 15:22:02 +0200

    chore: update environment configuration for staging and local development, changing API and web URLs

--- lib/config.ts
+++ lib/config.ts
@@ -14,15 +14,16 @@ const environments = {
     EXPO_PUBLIC_SUPABASE_URL: "https://sqzpncgvzsmgynpnpvpu.supabase.co",
     EXPO_PUBLIC_SUPABASE_ANON_KEY:
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNxenBuY2d2enNtZ3lucG5wdnB1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkzOTUwMTgsImV4cCI6MjA2NDk3MTAxOH0.Z_pXE8Y7jiMxQ8gUtqmk-30lZAK1sY-IhY2OK91t0ag",
-    EXPO_PUBLIC_WEB_URL: "https://getmovapp.com",
+    EXPO_PUBLIC_WEB_URL: "http://localhost:3001",
   },
   preview: {
     NODE_ENV: "preview",
-    EXPO_PUBLIC_API_URL: "https://mov-backend-production.up.railway.app",
+    EXPO_PUBLIC_API_URL: "https://mov-backend-staging.up.railway.app",
     EXPO_PUBLIC_SUPABASE_URL: "https://jwqcqpsatykgqhyxclbv.supabase.co",
     EXPO_PUBLIC_SUPABASE_ANON_KEY:
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp3cWNxcHNhdHlrZ3FoeXhjbGJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMwOTYzOTYsImV4cCI6MjA2ODY3MjM5Nn0.AkcJSc8dSdHA4CMI4ehBX4ypC4wJ37orZsQGzJstSDs",
-    EXPO_PUBLIC_WEB_URL: "https://getmovapp.com",
+    EXPO_PUBLIC_WEB_URL:
+      "https://mov-web-git-staging-alexis-projects-83365ee2.vercel.app",
   },
   production: {
     NODE_ENV: "production",
